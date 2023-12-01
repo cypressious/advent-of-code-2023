23428b3b4f6bb99fd0844078ea3e6111f752d6c3
Kirill Rakhman <kirill.rakhman@gmail.com>
2023-12-01 21:16:03 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,31 @@
 fun main() {
+    val digitWords =
+        listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            val digits = it.mapNotNull(Char::digitToIntOrNull)
+            digits.first() * 10 + digits.last()
+        }
     }
+
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val digits = line.indices
+                .mapNotNull { i ->
+                    line[i].digitToIntOrNull()
+                        ?: digitWords.indexOfFirst(line.substring(i)::startsWith)
+                            .takeUnless { it < 0 }
+                }
+            digits.first() * 10 + digits.last()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput2 = readInput("Day01_test2")
+    check(part1(testInput) == 142)
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
