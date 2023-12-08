f03db52b954b356fe1faa67235af4890b7e68f6c
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2023-12-08 09:27:28 +0100

    Day 7 part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -9,48 +9,71 @@ enum class HandType {
 }
 
 fun main() {
-    val labels = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
+    data class Entry(val hand: String, val bid: Int, val type: HandType)
 
-    data class Entry(val hand: String, val bid: Int) {
-        val counts = hand.groupingBy { it }.eachCount()
-        val type = when {
-            counts.size == 1 -> HandType.Five
-            counts.any { it.value == 4 } -> HandType.Four
-            counts.any { it.value == 3 } && counts.any { it.value == 2 } -> HandType.FullHouse
-            counts.any { it.value == 3 } -> HandType.Three
-            counts.size == 3 -> HandType.TwoPair
-            counts.size == 4 -> HandType.Pair
-            else -> HandType.HighCard
-        }
-
-        override fun toString() =
-            "Entry(hand='$hand', bid=$bid, counts=$counts, type=$type)"
-    }
+    fun comparator(labels: List<Char>) = compareByDescending<Entry> { it.type }
+        .thenByDescending { labels.indexOf(it.hand[0]) }
+        .thenByDescending { labels.indexOf(it.hand[1]) }
+        .thenByDescending { labels.indexOf(it.hand[2]) }
+        .thenByDescending { labels.indexOf(it.hand[3]) }
+        .thenByDescending { labels.indexOf(it.hand[4]) }
 
     fun part1(input: List<String>): Int {
-        val entries = input.mapTo(mutableListOf()) {
-            val (hand, bid) = it.split(" ")
-            Entry(hand, bid.toInt())
+        val entries = input.mapTo(mutableListOf()) { line ->
+            val (hand, bid) = line.split(" ")
+            val counts = hand.groupingBy { it }.eachCount()
+            val type = when {
+                counts.size == 1 -> HandType.Five
+                counts.any { it.value == 4 } -> HandType.Four
+                counts.any { it.value == 3 } && counts.any { it.value == 2 } -> HandType.FullHouse
+                counts.any { it.value == 3 } -> HandType.Three
+                counts.size == 3 -> HandType.TwoPair
+                counts.size == 4 -> HandType.Pair
+                else -> HandType.HighCard
+            }
+            Entry(hand, bid.toInt(), type)
         }
 
-        entries.sortWith(compareByDescending<Entry> { it.type }
-            .thenByDescending { labels.indexOf(it.hand[0]) }
-            .thenByDescending { labels.indexOf(it.hand[1]) }
-            .thenByDescending { labels.indexOf(it.hand[2]) }
-            .thenByDescending { labels.indexOf(it.hand[3]) }
-            .thenByDescending { labels.indexOf(it.hand[4]) }
+        entries.sortWith(
+            comparator(listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'))
         )
 
         return entries.withIndex().sumOf { (i, entry) -> (i + 1) * entry.bid }
     }
 
+    fun part2(input: List<String>): Long {
+        val entries = input.mapTo(mutableListOf()) { line ->
+            val (hand, bid) = line.split(" ")
+            val counts = hand.groupingBy { it }.eachCount()
+            val jokers = counts.getOrDefault('J', 0)
 
-    fun part2(input: List<String>): Int {
-        return input.size
+            fun Map.Entry<Char, Int>.canBe(result: Int): Boolean {
+                return value == result || key != 'J' && value + jokers == result
+            }
+
+            val type = when {
+                counts.any { it.canBe(5) } -> HandType.Five
+                counts.any { it.canBe(4) } -> HandType.Four
+                counts.size == 2 || counts.size == 3 && jokers > 0 -> HandType.FullHouse
+                counts.any { it.canBe(3) } -> HandType.Three
+                counts.size == 3 && jokers == 0 -> HandType.TwoPair
+                counts.any { it.canBe(2) } -> HandType.Pair
+                else -> HandType.HighCard
+            }
+            Entry(hand, bid.toInt(), type)
+        }
+
+        entries.sortWith(
+            comparator(listOf('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'))
+        )
+
+        return entries.withIndex().sumOf { (i, entry) -> (i + 1L) * entry.bid }
+
     }
 
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 6440)
+    check(part2(testInput) == 5905L)
 
     val input = readInput("Day07")
     part1(input).println()
